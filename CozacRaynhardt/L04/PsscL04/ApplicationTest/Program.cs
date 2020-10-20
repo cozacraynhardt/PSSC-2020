dd9595610202bb1eb7d17f87d5379a9507de3295
cozacraynhardt <62707254+cozacraynhardt@users.noreply.github.com>
2020-10-20 19:47:36 +0300

    Add files via upload

---
+++ CozacRaynhardt/L04/PsscL04/ApplicationTest/Program.cs
@@ -0,0 +1,67 @@
+﻿using DomainQuestion.CreateQuestionWorkflow;
+using System;
+using System.Collections.Generic;
+using static DomainQuestion.CreateQuestionWorkflow.CreateQuestionResult;
+
+namespace ApplicationTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cmd = new CreateQuestionCommand("How to add a reference to my project in VS2017?", "Issues","VisualStudio2017", "Fixed Issues in VisualStudio 2017");
+            var result = CreateQuestion(cmd);
+
+            result.Match(
+                    ProcessQuestionPublished,
+                    ProcessQuestionNotPublished,
+                    ProcessInvalidQuestion
+                );
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadLine();
+
+        }
+        private static ICreateQuestionResult ProcessInvalidQuestion(QuestionValidationFailed validationErrors)
+        {
+            Console.WriteLine("Question validation failed: ");
+            foreach (var error in validationErrors.ValidationErrors)
+            {
+                Console.WriteLine(error);
+            }
+            return validationErrors;
+        }
+        private static ICreateQuestionResult ProcessQuestionNotPublished(QuestionNotPublished questioneNotPublishedResult)
+        {
+            Console.WriteLine($"Question not published: {questioneNotPublishedResult.Reason}");
+            return questioneNotPublishedResult;
+        }
+
+        private static ICreateQuestionResult ProcessQuestionPublished(QuestionPublished question)
+        {
+            var cmdUser = new CreateUserCommand("CozacRayni", "rayni.cozac@yahoo.com");
+            Console.WriteLine($"Question from {cmdUser.Username} was accepted!");
+            Console.WriteLine($"Question was published with id: {question.QuestionId}");
+            Console.WriteLine($"Confirmation email was sent to: {cmdUser.Email}");
+            return question;
+        }
+        public static ICreateQuestionResult CreateQuestion(CreateQuestionCommand createQuestionCommand)
+        {
+            if (string.IsNullOrWhiteSpace(createQuestionCommand.Category) || string.IsNullOrWhiteSpace(createQuestionCommand.Title))
+            {
+                var errors = new List<string>() { "Invalid category or title!" };
+                return new QuestionValidationFailed(errors);
+            }
+
+            if (new Random().Next(7) > 1)
+            {
+                return new QuestionNotPublished("Category or title could not be verified");
+            }
+
+            var questionId = Guid.NewGuid();
+            var result = new QuestionPublished(questionId, createQuestionCommand.Category, createQuestionCommand.Title);
+
+            return result;
+        }
+    }
+}
---
+++ CozacRaynhardt/L04/PsscL04/DomainProfile/CreateQuestionWorkflow/CreateQuestionCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DomainQuestion.CreateQuestionWorkflow
+{
+    public struct CreateQuestionCommand
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public string Tags { get; set; }
+        public string Topic { get; set; }
+
+        public CreateQuestionCommand(string title,string category,string tags,string topic)
+        {
+            Title = title;
+            Category = category;
+            Tags = tags;
+            Topic = topic;
+        }
+    };
+    
+}
